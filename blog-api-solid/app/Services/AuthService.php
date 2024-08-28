2df6f3bd8efbde914a464cbda9947ce6952015c4
Ceeh-Jay <chiemekaelumeziem@gmail.com>
2024-08-28 18:51:52 +0100

    add AuthService

---
+++ blog-api-solid/app/Services/AuthService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthService
+{
+    public function register(array $data)
+    {
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = new User();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+        $user->save();
+
+        return $user;
+    }
+
+    public function login(array $credentials)
+    {
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTAuth::factory()->getTTL() * 60
+        ];
+    }
+}
